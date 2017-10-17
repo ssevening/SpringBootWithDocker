4b7e8946404fb9c363beaf5cc878caeb62cca2ab
赛文 <chenxing.pancx@alibaba-inc.com>
2017-10-17 19:30:46 +0800

    添加定时器功能，监控百度网站有没有挂掉。

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,7 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+@EnableScheduling // 开启定时执行任务功能
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/hello/ScheduledTest.java
@@ -0,0 +1,89 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.swing.plaf.TextUI;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+/**
+ * // 每天早八点到晚八点，间隔2分钟执行任务
+ *
+ * @Scheduled(cron="0 0/2 8-20 * * ?")
+ * // 每天早八点到晚八点，间隔3分钟执行任务
+ * @Scheduled(cron="0 0/3 8-20 * * ?")
+ * // 每天早八点到晚八点，间隔1分钟执行任务
+ * @Scheduled(cron="0 0/1 8-20 * * ?")
+ */
+@Component
+public class ScheduledTest {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTest.class);
+
+    @Scheduled(cron = "0 0/2 8-20 * * ?")
+    public void executeFileDownLoadTask() {
+
+        // 间隔2分钟,执行工单上传任务
+        Thread current = Thread.currentThread();
+        System.out.println("定时任务1:" + current.getId());
+        logger.info("ScheduledTest.executeFileDownLoadTask 定时任务1:" + current.getId() + ",name:" + current.getName());
+
+    }
+
+    @Scheduled(cron = "0 0/1 8-20 * * ?")
+    public void executeUploadTask() {
+
+        // 间隔1分钟,执行工单上传任务
+        Thread current = Thread.currentThread();
+        System.out.println("定时任务2:" + current.getId());
+        logger.info("ScheduledTest.executeUploadTask 定时任务2:" + current.getId() + ",name:" + current.getName());
+        // 获取百度的数据
+        String data = getDataFromInternet("http://www.baidu.com");
+        logger.info(data);
+        if (data != null || data.length() == 0) {
+            // 喜大普奔，百度的网站打不开啦！
+            // send mail to admin@baidu.com
+        }
+    }
+
+    @Scheduled(cron = "0 0/3 5-23 * * ?")
+    public void executeUploadBackTask() {
+
+        // 间隔3分钟,执行工单上传任务
+        Thread current = Thread.currentThread();
+        System.out.println("定时任务3:" + current.getId());
+        logger.info("ScheduledTest.executeUploadBackTask 定时任务3:" + current.getId() + ",name:" + current.getName());
+    }
+
+
+    public static String getDataFromInternet(String path) {
+        try {
+            URL url = new URL(path.trim());
+            //打开连接
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            if (200 == urlConnection.getResponseCode()) {
+                //得到输入流
+                InputStream is = urlConnection.getInputStream();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int len = 0;
+                while (-1 != (len = is.read(buffer))) {
+                    baos.write(buffer, 0, len);
+                    baos.flush();
+                }
+                return baos.toString("utf-8");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
